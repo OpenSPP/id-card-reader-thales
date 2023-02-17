d14113646db0c4fe5d582db1f4faaefb65b1d582
Hieu Nguyen <hieu@newlogic.com>
2023-02-17 13:54:35 +0700

    add custom response headers

--- Program.cs
+++ Program.cs
@@ -14,6 +14,17 @@ builder.Host.UseWindowsService();
 
 var app = builder.Build();
 
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Add("Access-Control-Allow-Origin", "*");
+    context.Response.Headers.Add("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+    context.Response.Headers.Add("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
+    context.Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
+    context.Response.Headers.Add("Pragma", "no-cache");
+    context.Response.Headers.Add("Expires", "0");
+    await next();
+});
+
 app.MapGet("/", () => "ID Card Reader Thales");
 app.MapGet("/initialise", API.Initialise);
 app.MapGet("/shutdown", API.Shutdown);
